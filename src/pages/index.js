102a3d6409c4991b80fa735e9833454f6df34d27
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2023-04-25 17:27:01 +0700

    mengambil data

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,30 @@ import prisma from "../../lib/prisma";
 
 export const getStaticProps = async () => {
   const data = await prisma.user.findMany();
-  // console.log(data);
+  console.log(data);
   return {
-    props: { data: JSON.stringify(data, null, 2) },
+    props: {
+      data: JSON.stringify(data),
+    },
   };
 };
 
 export default function Home(props) {
   return (
     <div>
-      {/* <h1>hello</h1> */}
-      {/* query data prisma */}
-      <pre>{props.data}</pre>
+      {JSON.parse(props.data).map((user) => (
+        <div key={user.id}>
+          <h1>{user.id}</h1>
+          <h1>{user.name}</h1>
+          <h1>
+            {new Date(user.createdAt).toLocaleDateString("en-GB", {
+              day: "numeric",
+              month: "long",
+              year: "numeric",
+            })}
+          </h1>
+        </div>
+      ))}
     </div>
   );
 }
